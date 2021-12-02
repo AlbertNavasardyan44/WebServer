5d997eeb0e3db1085807789622e28d2133736eaf
AlbertNavasardyan44 <albertnavasard32@gmail.com>
2021-12-02 16:47:07 +0400

    commit

--- index.js
+++ index.js
@@ -1,47 +1,54 @@
-const express=require('express');
+const express = require('express');
 const mongoose = require("mongoose");
 
-const app=express();
-const ejs=require('ejs');
-const PORT=process.env.PORT || 3000;
+const app = express();
+const ejs = require('ejs');
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
-app.use( express.static( "views" ) );
-//const Person = require("./Models/Person")
+app.use(express.static("views"));
 const uri = process.env.MONGODB_URI;
 mongoose
-.connect(
-   ' mongodb+srv://albertdb:albertdb449@cluster0.wyknk.mongodb.net/sampletest?retryWrites=true&w=majority',
-   {
-       useNewUrlParser:true,
-   }
-   )
-   .then(()=>console.log('mongodb conected'))
-   .catch(err=>console.log(err));
+    .connect(
+        ' mongodb+srv://albertdb:albertdb449@cluster0.wyknk.mongodb.net/sampletest?retryWrites=true&w=majority',
+        {
+            useNewUrlParser: true,
+        }
+    )
+    .then(() => console.log('mongodb conected'))
+    .catch(err => console.log(err));
 
 
-const UsersSchema=new mongoose.Schema({
+const UsersSchema = new mongoose.Schema({
     imgname: {
-       type: String,
-        required : true,
+        type: String,
+        required: true,
     }
 });
-   app.get('/', (req, res) => {
-          Users.create({
-              imgname:'2.jpg', 
-          })
 
-        .then((users) => res.send(users))
-        .catch((err )=> res.send(err));
-   
+
+const Users = mongoose.model('Users', UsersSchema);
+
+app.get('/addDefaultImages', (req, res) => {
+   const arr = [{ imgname: '3.jpg' },{ imgname: '4.jpg' },{ imgname: '5.jpg' }];
+Users.insertMany(arr, function(err, users) {
+    // Users.create({
+    //     imgname:'4.jpg',
+    // })
+    if (err) throw err;
+            console.log("Number of documents inserted: " + res.insertedCount);
+            
+})
+        .then(user => res.send(user))
+        .catch(err => res.send(err));
+
 });
+// const Users = mongoose.model('Users', UsersSchema);
 
-const Users=mongoose.model('Users', UsersSchema);
-  
-app.get('/',(req, res)=>{
-    Users.find({},function(err,users){
-        res.render('index',{
-            imageslist:users
+app.get('/', (req, res) => {
+    Users.find({}, function (err, users) {
+        res.render('index', {
+            imageslist: users
         })
     })
 })
@@ -54,6 +61,6 @@ app.get('/',(req, res)=>{
 
 
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log('server has been started...')
 })
